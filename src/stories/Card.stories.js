31acf70626cf1631348f5d520f8e0649f4fc39c5
Adrianna Guevarra <adrianna.guevarra@sdvi.com>
2022-10-27 18:08:19 -0700

    enable card story to take in props and view them right away

---
+++ src/stories/Card.stories.js
@@ -0,0 +1,20 @@
+import Card from '../components/Card.vue';
+
+export default {
+    title: 'Card',
+    component: Card,
+}
+
+const Template = (args) => ({
+    components: { Card },
+    setup() {
+      //👇 The args will now be passed down to the template
+      return { args };
+    },
+    template: '<Card v-bind="args" />',
+  });
+
+  export const Primary = Template.bind({});
+  Primary.args = {
+    name: 'Adrianna'
+  }
--- src/stories/HelloWorld.stories.js
+++ src/stories/HelloWorld.stories.js
@@ -1,4 +1,4 @@
-import HelloWorld from './HelloWorld.vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
     title: 'HelloWorld',
